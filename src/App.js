9f7c4c130370187ce87c0d2548e24e5104d87bde
Michał Płużek <michalpluzek@gmail.com>
2021-12-07 11:26:11 +0100

    Add form with handlers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+import Form from "./Form";
+import List from "./List";
+
+import store from "./store/store";
+
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello, World!</div>;
-}
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div>
+        <Form />
+        <p>Redux</p>
+        <List />
+      </div>
+    </Provider>
+  );
+};
 
 export default App;
---
+++ src/Form.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const Form = ({ author = "", comment = "", id, rate = 0 }) => {
+  const [authorInput, setAuthorInput] = useState(author);
+  const [commentInput, setCommentInput] = useState(comment);
+  const [rateInput, setRateInput] = useState(rate);
+
+  const handleAuthorChange = (e) => setAuthorInput(e.target.value);
+
+  const handleCommentChange = (e) => setCommentInput(e.target.value);
+
+  const handleRateChange = (e) => setRateInput(e.target.value);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (!authorInput.length) return;
+
+    const rateObject = {
+      author: authorInput,
+      comment: commentInput,
+      id,
+      rate: Number(rateInput),
+    };
+  };
+
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <div>
+        <label>
+          Autor:
+          <input
+            onChange={handleAuthorChange}
+            type="text"
+            value={authorInput}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Ocena:
+          <input onChange={handleRateChange} type="number" value={rateInput} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Komentarz:
+          <input
+            onChange={handleCommentChange}
+            type="text"
+            value={commentInput}
+          />
+        </label>
+      </div>
+      <button type="submit">{id ? "Edycja oceny" : "Dodaj ocenę"}</button>
+    </form>
+  );
+};
+
+export default Form;
