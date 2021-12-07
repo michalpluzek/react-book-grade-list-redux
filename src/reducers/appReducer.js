c61ffd4d7dcd670fe665440e8c1732d6cfd25f85
Michał Płużek <michalpluzek@gmail.com>
2021-12-07 10:19:27 +0100

    Add app reducer

---
+++ src/reducers/appReducer.js
@@ -0,0 +1,27 @@
+import { ADD, DELETE, EDIT } from "../actions/appActions";
+
+const appReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [...state, action.payload];
+
+    case DELETE:
+      return state.filter((rate) => rate.id !== action.payload.id);
+
+    case EDIT:
+      return state.map((rate) =>
+        rate.id !== action.payload.id
+          ? rate
+          : {
+              author: action.payload.author,
+              comment: action.payload.comment,
+              id: rate.id,
+              rate: action.payload.rate,
+            }
+      );
+
+    default:
+      console.warn(`Nie obsługiwana akcja typu: ${action.type}`);
+      return state;
+  }
+};
