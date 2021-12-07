f35a05bcd2c91368b736fba9f72d85f5407706db
Michał Płużek <michalpluzek@gmail.com>
2021-12-07 10:04:42 +0100

    Add actions

---
+++ src/actions/appActions.js
@@ -0,0 +1,30 @@
+export const ADD = "ADD";
+export const DELETE = "DELETE";
+export const EDIT = "EDIT";
+
+const addRate = ({ author, comment, rate }) => ({
+  type: ADD,
+  payload: {
+    author,
+    comment,
+    id: Math.floor(Math.random * 1000),
+    rate,
+  },
+});
+
+const deleteRate = (id) => ({
+  type: DELETE,
+  payload: {
+    id,
+  },
+});
+
+const editRate = ({ id, author, comment, rate }) => ({
+  type: EDIT,
+  payload: {
+    author,
+    comment,
+    id,
+    rate,
+  },
+});
